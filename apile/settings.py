7d3c295a6234dc2ce9dc71dca6691db5dce6120d
faxedexcel <chinh.le23@gmail.com>
2019-03-18 21:08:41 -0400

    updated apile/settings.py to PostgreSQL and installed psycopg2

--- apile/settings.py
+++ apile/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'apile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # updated 3/18/2019 by Chinh for PostgreSQL setup
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apile',
+        'USER': 'apile',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
