4f3736560814918e1e2b1b72013fe53eb122a647
dmpeterson317 <dmpeterson317@gmail.com>
2019-03-19 22:14:38 -0400

    create jank view for index and create jank template for base and index

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', PostList.as_view(), name='index')
+    path('', views.PostList.as_view(), name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import Post
 
-class PostList(ListView):
+class PostList(generic.ListView):
     model = Post
     context_object_name = 'posts' #default context object name is object_list
     template_name = 'index.html'
