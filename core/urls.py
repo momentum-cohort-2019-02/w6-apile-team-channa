318a592fc04f1a5c6473be4a92fdd932b9f4cbaf
faxedexcel <chinh.le23@gmail.com>
2019-03-21 11:33:01 -0400

    update urls and views to link up vote button on index.html

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('post/<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:post_pk>/vote/', views.post_vote_view, name="post_vote"),
+        # intermediary path to display a vote and thus not specific to a template   
 ]
